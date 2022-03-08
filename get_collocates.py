c0b355fd5b630c03aeae9861dda80b13d27292f7
ayla <aylataylor@gmail.com>
2022-03-08 11:59:29 -0500

    divided between 0 and 2nd order adjs

--- get_collocates.py
+++ get_collocates.py
@@ -1,33 +1,53 @@
 import json
+import click
 from collections import Counter, defaultdict
 
-TARGET = 'dried'
+TARGETS = {'blackened', 'cleaned', 'cleared', 'cooled', 'deepen', 'dirtied', 'dried', 'hardened', 'toughened',
+           'enlarged', 'lengthened', 'reddened', 'sharpened', 'shortened', 'smoothed', 'softened', 'straightened',
+           'strengthened', 'sweetened', 'tighten', 'warmed', 'weakened', 'whitened', 'widened'}
 
 
+@click.command()
+@click.argument("json_file", type=click.Path(exists=True))
+@click.argument("json_out", type=click.Path())
 def main(json_file: str, json_out: str):
-    collocates = []
+    collocates = defaultdict(list)
     nouns = []
-    with open(json_file, encoding='utf8') as f:
+    word_counts = defaultdict(int)
+    head_dict = defaultdict(lambda: defaultdict(int))
+    with open(json_file, 'r', encoding='utf8') as f:
         for line in f:
             word_dicts = json.loads(line)
             for i, w in enumerate(word_dicts):
-                if w["text"] == TARGET:
-                    if i > 0:
-                        if word_dicts[i+1]['upos'] == 'NOUN':
-                            next_w = word_dicts[i+1]['text']
-                            nouns.append(next_w)
-                        seq = dict()
-                        seq['text'] = word_dicts[i-1]['text'] + ' ' + w['text'] + ' ' + word_dicts[i+1]['text']
-                        seq['full parse'] = (word_dicts[i-1], w, word_dicts[i+1])
-                        collocates.append(seq)
+                if w["text"] in TARGETS:
+                    text = ''
+                    for word in word_dicts[:i]:
+                        text += word['text'] + ' '
+                    text += '[' + w['text'] + ']' + ' '
+                    for word in word_dicts[i+1:]:
+                        text += word['text'] + ' '
+                    head_index = w['head']
+                    head = word_dicts[head_index - 1]
+                    head_dict[w['text']][head['text']] += 1
+                    nouns.append(head['text'])
+                    seq = dict()
+                    print(text)
+                    print(head)
+                    seq['text'] = text
+                    # seq['full parse'] = word_dicts
+                    seq['head'] = head['text']
+                    # print(seq)
+                    collocates[w['text']] += seq
+                    # print(collocates)
     noun_counts = Counter(nouns)
-    print(nouns)
+    # print(head_dict)
+    # print(nouns)
     with open(json_out, 'w', encoding='utf8') as out_f:
-        print(noun_counts)
+        # print(noun_counts)
         for x in collocates:
             # print(x)
             out_f.write(json.dumps(x))
 
 
 if __name__ == '__main__':
-    main('extracted_mini.json', 'out.txt')
+    main()
--- get_derived_adj.py
+++ get_derived_adj.py
@@ -5,12 +5,11 @@ import click
 import attrs
 
 ADVERBS = {'recently', 'perfectly', 'completely', 'badly', 'quickly'}
-NOT_AFTER = {'dried': {'out', 'up'}}
+NOT_AFTER = {'out', 'up'}
 NOT_BEFORE = {}
-
-TARGET_DERIVED = {'dried'}
-TARGET_ROOT_ADJ = {'dry'}
-
+TARGETS = {'blackened', 'cleaned', 'cleared', 'cooled', 'deepen', 'dirtied', 'dried', 'hardened', 'toughened',
+           'enlarged', 'lengthened', 'reddened', 'sharpened', 'shortened', 'smoothed', 'softened', 'straightened',
+           'strengthened', 'sweetened', 'tighten', 'warmed', 'weakened', 'whitened', 'widened'}
 
 
 @attrs.define
@@ -23,38 +22,32 @@ class Word:
 
 
 def is_derived(sent: list[dict]):
-    target = 'dried'
     words = dict()
     for word in sent:
         words[int(word['id'])] = Word(word['id'], word['text'], word['upos'], word['xpos'], word['head'])
     for i, word in words.items():
-        if word.text == target and word.upos == 'VERB':
-            if i + 1 < len(words) and words[i+1].text in NOT_AFTER[target]:
+        if word.text in TARGETS and word.head != 0 and words[word.head].upos == 'NOUN':
+            if i + 1 < len(words) and words[i+1].text in NOT_AFTER:
                 return False
-            if i > 1 and words[i-1].upos == 'DET':
+            else:
                 return True
-            if i > 2 and words[i-1].upos == 'ADV' and words[i-2].upos == 'DET':
-                # if i + 1 < len(words) and words[i - 1].upos == 'NOUN':
-                return True
-            # if i + 1 < len(words) and words[i-1].upos == 'NOUN':
-            #     if
 
 
-# @click.command()
-# @click.argument("json_file", type=click.Path(exists=True))
-# @click.argument("out_jsonl_file", type=click.Path())
+@click.command()
+@click.argument("json_file", type=click.Path(exists=True))
+@click.argument("out_jsonl_file", type=click.Path())
 def main(json_file: str, out_jsonl_file: str):
     selected = []
     with open(json_file, encoding='utf8') as f:
-        for line in f:
+        for line in tqdm.tqdm(f):
             line_dict = json.loads(line)
             for sent in line_dict['parsed']:
                 if is_derived(sent):
                     selected.append(sent)
     with open(out_jsonl_file, "w", encoding="utf-8") as out_f:
-        for x in selected:
+        for x in tqdm.tqdm(selected):
             out_f.write(json.dumps(x) + '\n')
 
 
 if __name__ == "__main__":
-    main('pos_tagged_mini.json', 'extracted_mini.json')
+    main()
