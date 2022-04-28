d0762fd3a27ce6e88c062f9a67d77ab0a8bc3f14
ayla <aylataylor@gmail.com>
2022-04-28 01:13:16 -0400

    started manual clustering

--- csi_match.py
+++ csi_match.py
@@ -10,7 +10,6 @@ def get_synset(words: set[str]) -> dict:
     synset_dict = dict()
     for word in words:
         synsets = wn.synsets(word, pos=wn.ADJ)
-        # print(word, synsets)
         synset_dict[word] = synsets
     return synset_dict
 
@@ -20,12 +19,11 @@ def read_to_set(filename: str) -> set[str]:
     with open(filename, encoding='utf-8') as f:
         for line in f:
             word_set.add(line.strip())
-    # print(word_set)
     return word_set
 
 
 def main():
-    csi_inventory = read_to_set('csi_data/inventory.txt')
+    # csi_inventory = read_to_set('csi_data/inventory.txt')
     target_words = read_to_set(sys.argv[1])
     word_dict = get_synset(target_words)
     csi_match_dict = dict()
@@ -46,11 +44,7 @@ def main():
                     print(synset.definition())
                 else:
                     print('\t'.join([word, "[None]", synset.definition(), str(synset.examples())]), file=out_f)
-
-                    # print("skipped", word, synset)
                     unable_to_find.append((word, synset))
-            # print(csi_match_dict[synset])
-        # print(word, word_dict[word])
 
 
 if __name__ == '__main__':
