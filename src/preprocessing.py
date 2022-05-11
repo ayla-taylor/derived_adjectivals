9eaf25cd6d9dc5fe3875f2ef61c3415416a8430c
ayla <aylataylor@gmail.com>
2022-05-11 18:48:07 -0400

    preprocessing script

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,29 +1,83 @@
 import argparse
-from preprocessing_utils import read_sense_dict
+import random
 
+import pandas as pd
 
-parser = argparse.ArgumentParser(description="Preprocess data for various models")
+from preprocessing_utils import read_sense_dict, read_file, root_to_derived, get_head
 
-parser.add_argument('model', type=str, default='baseline', help='which model is this preprocessing for')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description="Preprocess data for various models")
+#
+# parser.add_argument('model', type=str, default='baseline', help='which model is this preprocessing for')
+# args = parser.parse_args()
 
 SENSE_DICT_FILE = "../data/sense_dict.json"
 
 
-def process_datafiles(datafiles: list[str]) -> None:
+def process_embed_data(datafiles: list[list[str]]) -> None:
     filepath = '../data/'
-    for data in datafiles:
+
+
+def process_model_files(datafiles: list[str], sense_dict: dict) -> None:
+    filepath = '../data/Annotated/'
+    sentence_dicts = []
+    for datafile in datafiles:
+        data = read_file(filepath + datafile)
+        target_word = data['dataset']['name']
+        sentences = data['examples']
+        for sent in sentences:
+            sentence_dict = {}
+            data_sentence = sent['content']
+            if sent['classifications']:
+                for classification in sent['classifications']:
+                    if classification['correct']:
+                        tag = classification['classname']
+                        for sense_tag, sense_def in sense_dict[target_word].items():
+                            sentence_dict['text1'], sentence_dict['text2'] = data_sentence, sense_def
+                            if sense_tag == tag:
+                                sentence_dict['label'] = 1
+                            else:
+                                sentence_dict['label'] = 0
+            if sent['annotations']:
+                for annotation in sent['annotations']:
+                    if annotation['correct']:
+                        sentence_dict['derived_pair'] = root_to_derived[target_word] + ' ' + annotation['value']
+            else:
+                sentence_dict['derived_pair'] = get_head(data_sentence, target_word)
+            sentence_dicts.append(sentence_dict)
+    random.shuffle(sentence_dicts)
+    models_sentences = sentence_dicts[:len(sentence_dicts)], sentence_dicts[len(sentence_dicts):]
+    for sent in models_sentences[1]:
+        sent['text1'] = sent['derived_pair'] + ' | ' + sent['text1']
+    split_for_each_model = []
+    for model in models_sentences:
+        train = pd.DataFrame(model[:int(len(model) * .8)], columns=['text1', 'text2', 'label'])
+        dev = pd.DataFrame(model[int(len(model) * .8):int(len(sentence_dicts) * .9)],
+                           columns=['text1', 'text2', 'label'])
+        test = pd.DataFrame(model[-int(len(model) * .1):], columns=['text1', 'text2', 'label'])
+        split_for_each_model.append({'train': train, 'dev': dev, 'test': test})
+    baseline_split, fullmodel_split = split_for_each_model
+    for split_name, split_df in baseline_split.items():
+        split_df.to_csv('../data/baseline/' + split_name + '.csv', index=False)
+    for split_name, split_df in fullmodel_split.items():
+        split_df.to_csv('../data/full_model/' + split_name + '.csv', index=False)
 
 
 def main():
     sense_dict = read_sense_dict(SENSE_DICT_FILE)
-    if args.model == "baseline":
-        datafiles = []
-    elif args.model == "derived_embed":
-        datafiles = []
-    elif args.model == "full_model":
-        datafiles = []
-    process_datafiles(datafiles)
+
+    if args.embed_data:
+        datafiles = [['cooled-annotation-2_annotations.json',
+                      'blackened-annotation_annotations.json',
+                      'softened-annotation_annotations.json'],
+                     ['cooled-sense-annotation-only_annotations.json',
+                      'hardened-sense-annotation_annotations.json']]
+        process_embed_data(datafiles)
+    else:
+        datafiles = ['cool-annotation_annotations.json',
+                     'hard-annotation_annotations.json',
+                     'black-annotation_annotations.json']
+        process_model_files(datafiles, sense_dict)
+
 
 if __name__ == '__main__':
     main()
--- src/preprocessing_utils.py
+++ src/preprocessing_utils.py
@@ -1,9 +1,38 @@
 import json
+import spacy
+import random
+import pandas as pd
+
+
+root_to_derived = {'cool': 'cooled',
+                   'hard': 'hardened',
+                   'black': 'blackened'}
+
+spacy_nlp = spacy.load("en_core_web_sm",
+                       exclude=["ner", "attribute_ruler", "lemmatizer"])
 
 
 def read_sense_dict(filename: str) -> dict:
-    with open(filename, emcoding='utf8') as f:
+    with open(filename, 'r', encoding='utf8') as f:
         sense_dict = json.load(f)
     return sense_dict
 
+
+def read_file(filename: str) -> dict:
+    with open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            data = json.loads(line)
+    return data
+
+
+def get_head(sent: str, target_word) -> str:
+    parsed_doc = spacy_nlp(sent)
+    print(parsed_doc)
+    bracketed_target = '[' + target_word + ']'
+    for i, tok in enumerate(parsed_doc[1:-1]):
+        bracketed_word = ''.join(parsed_doc[i-1:i+1])
+        if bracketed_word == bracketed_target:
+            target = tok
+            head = target.head
+    return root_to_derived[target.text] + ' ' + head.text
 
