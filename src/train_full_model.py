3f48d72dbbad2795a5dd369ff3c0de185456a623
ayla <aylataylor@gmail.com>
2022-05-12 13:55:53 -0400

    fix model issue

--- src/train_full_model.py
+++ src/train_full_model.py
@@ -98,4 +98,25 @@ for epoch in range(num_epochs):
 #     print('Accuracy: {} %'.format(100 * correct / total))
 
 # Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+# torch.save(model.state_dict(), 'model.ckpt')
+
+
+labels = torch.load('eval_labels.pt')
+inputs = torch.load('eval_embed.pt')
+
+labels = labels.to(device)
+inputs = inputs.to(device)
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for i in range(0, labels.shape[0], batch_size):
+        batched_inputs = inputs[i: i + batch_size]
+        batched_labels = labels[i: i + batch_size]
+        # images = images.reshape(-1, 28 * 28).to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy: {} %'.format(100 * correct / total))
